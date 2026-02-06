b32d45935733de59ab17d1ead9d0e726bb4b1315
Tahmim Shibli <shibli05@gmail.com>
2026-02-06 22:23:08 +0100

    chore: initialize astro project and config

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://shibli049.github.io',
+    base: '/sfi',
+    outDir: './dist',
+});
